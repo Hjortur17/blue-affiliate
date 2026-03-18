92b47d50d87d99edd6919215e05298ceb1e18f55
Hjörtur Freyr <hjorturfreyr@hjorturfreyr.com>
2026-03-18 11:02:37 +0000

    🚀 Added - Sitebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,38 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "./ui/accordion";
+
+import { IconComponent, Icon } from "./Icon";
+
+import { cn } from "@/lib/utils";
+
+const navigation: {
+  name: string;
+  href: string;
+  icon: keyof typeof Icon;
+  current: boolean;
+}[] = [
+  { name: "Dashboard", href: "#", icon: "Car", current: true },
+  { name: "Performance", href: "#", icon: "Graph", current: false },
+  {
+    name: "Marketing material",
+    href: "#",
+    icon: "LongArrowRight",
+    current: false,
+  },
+  { name: "Academy", href: "#", icon: "GraduationCap", current: false },
+  {
+    name: "Request payout",
+    href: "#",
+    icon: "Information",
+    current: false,
+  },
+  { name: "Log out", href: "#", icon: "LogOut", current: false },
+];
+
 const accordion = [
   {
     title: "Price",
@@ -24,13 +59,51 @@ const accordion = [
 export default function Sidebar() {
   return (
     <aside className="col-span-4 space-y-2.5">
-      <div className="border border-light-gray rounded-2xl px-8 py-5"></div>
-
-      <div className="border border-light-gray rounded-2xl px-8 py-5">
-        <p className="font-medium text-2xl mb-2.5">Good to know</p>
+      <div className="border border-light-gray rounded-2xl p-8">
+        <nav className="flex flex-1 flex-col">
+          <ul role="list" className="-mx-2 space-y-1">
+            {navigation.map((item) => {
+              return (
+                <li key={item.name}>
+                  <a
+                    href={item.href}
+                    className={cn(
+                      item.current &&
+                        "h-15 flex items-center bg-light-gray/50 rounded-r-xl before:content-[''] before:absolute before:inset-y-0 before:left-0 before:h-full before:w-2 before:bg-secondary",
+                      "hover:bg-light-gray/50 group flex gap-x-3.5 py-3 px-11 font-medium rounded-r-xl relative",
+                    )}
+                  >
+                    <IconComponent
+                      icon={item.icon}
+                      className="shrink-0 size-7"
+                    />
+                    {item.name}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
       </div>
 
-      <div className="border border-light-gray rounded-2xl px-8 py-5">
+      <div className="border border-light-gray rounded-2xl p-8">
+        <p className="font-medium text-2xl mb-2.5">Good to know</p>
+
+        <Accordion defaultValue={[accordion[0].title]}>
+          {accordion.map((item) => (
+            <AccordionItem key={item.title} value={item.title}>
+              <AccordionTrigger className="text-base font-medium py-4">
+                {item.title}
+              </AccordionTrigger>
+              <AccordionContent className="text-xs">
+                {item.content}
+              </AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </div>
+
+      <div className="border border-light-gray rounded-2xl p-8">
         <p className="font-medium text-2xl mb-2.5">Your affiliate link</p>
         <a href="#" className="www.bluecarrental.is/sfvero" target="_blank">
           www.bluecarrental.is/sfvero
